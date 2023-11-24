243edea949243b400fee167532925c885ca262ae
Mauro-Benitez <mauro.cbenitez@gmail.com>
2023-11-24 09:49:39 -0300

    Comecando a implementacao da funcao gerar relatorio

--- Forms/f_Relatorio.Designer.cs
+++ Forms/f_Relatorio.Designer.cs
@@ -30,14 +30,9 @@
         {
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.checkBoxNomeFuncionario = new System.Windows.Forms.CheckBox();
-            this.checkBoxIDFuncionario = new System.Windows.Forms.CheckBox();
-            this.checkBoxGastoBruto = new System.Windows.Forms.CheckBox();
-            this.checkBoxGastoLiquido = new System.Windows.Forms.CheckBox();
             this.checkBoxGastoINSS = new System.Windows.Forms.CheckBox();
             this.checkBoxGastoVT = new System.Windows.Forms.CheckBox();
             this.checkBoxGastoVR = new System.Windows.Forms.CheckBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.btnGerar = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -62,66 +57,23 @@
             this.label10.Size = new System.Drawing.Size(0, 16);
             this.label10.TabIndex = 56;
             // 
-            // checkBoxNomeFuncionario
-            // 
-            this.checkBoxNomeFuncionario.AutoSize = true;
-            this.checkBoxNomeFuncionario.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBoxNomeFuncionario.Location = new System.Drawing.Point(51, 114);
-            this.checkBoxNomeFuncionario.Name = "checkBoxNomeFuncionario";
-            this.checkBoxNomeFuncionario.Size = new System.Drawing.Size(267, 29);
-            this.checkBoxNomeFuncionario.TabIndex = 58;
-            this.checkBoxNomeFuncionario.Text = "Exibi nome dos Funcionário";
-            this.checkBoxNomeFuncionario.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxIDFuncionario
-            // 
-            this.checkBoxIDFuncionario.AutoSize = true;
-            this.checkBoxIDFuncionario.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBoxIDFuncionario.Location = new System.Drawing.Point(51, 152);
-            this.checkBoxIDFuncionario.Name = "checkBoxIDFuncionario";
-            this.checkBoxIDFuncionario.Size = new System.Drawing.Size(252, 29);
-            this.checkBoxIDFuncionario.TabIndex = 59;
-            this.checkBoxIDFuncionario.Text = "Exibir ID dos Funcionários";
-            this.checkBoxIDFuncionario.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxGastoBruto
-            // 
-            this.checkBoxGastoBruto.AutoSize = true;
-            this.checkBoxGastoBruto.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBoxGastoBruto.Location = new System.Drawing.Point(51, 281);
-            this.checkBoxGastoBruto.Name = "checkBoxGastoBruto";
-            this.checkBoxGastoBruto.Size = new System.Drawing.Size(178, 29);
-            this.checkBoxGastoBruto.TabIndex = 60;
-            this.checkBoxGastoBruto.Text = "Gasto Bruto Total";
-            this.checkBoxGastoBruto.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxGastoLiquido
-            // 
-            this.checkBoxGastoLiquido.AutoSize = true;
-            this.checkBoxGastoLiquido.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBoxGastoLiquido.Location = new System.Drawing.Point(51, 319);
-            this.checkBoxGastoLiquido.Name = "checkBoxGastoLiquido";
-            this.checkBoxGastoLiquido.Size = new System.Drawing.Size(252, 29);
-            this.checkBoxGastoLiquido.TabIndex = 61;
-            this.checkBoxGastoLiquido.Text = "Gastos em Salário Líquido";
-            this.checkBoxGastoLiquido.UseVisualStyleBackColor = true;
-            // 
             // checkBoxGastoINSS
             // 
             this.checkBoxGastoINSS.AutoSize = true;
             this.checkBoxGastoINSS.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBoxGastoINSS.Location = new System.Drawing.Point(51, 357);
+            this.checkBoxGastoINSS.Location = new System.Drawing.Point(51, 183);
             this.checkBoxGastoINSS.Name = "checkBoxGastoINSS";
             this.checkBoxGastoINSS.Size = new System.Drawing.Size(165, 29);
             this.checkBoxGastoINSS.TabIndex = 62;
             this.checkBoxGastoINSS.Text = "Gastos em INSS";
             this.checkBoxGastoINSS.UseVisualStyleBackColor = true;
+            this.checkBoxGastoINSS.CheckedChanged += new System.EventHandler(this.checkBoxGastoINSS_CheckedChanged);
             // 
             // checkBoxGastoVT
             // 
             this.checkBoxGastoVT.AutoSize = true;
             this.checkBoxGastoVT.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBoxGastoVT.Location = new System.Drawing.Point(51, 395);
+            this.checkBoxGastoVT.Location = new System.Drawing.Point(51, 221);
             this.checkBoxGastoVT.Name = "checkBoxGastoVT";
             this.checkBoxGastoVT.Size = new System.Drawing.Size(148, 29);
             this.checkBoxGastoVT.TabIndex = 63;
@@ -132,24 +84,12 @@
             // 
             this.checkBoxGastoVR.AutoSize = true;
             this.checkBoxGastoVR.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBoxGastoVR.Location = new System.Drawing.Point(51, 433);
+            this.checkBoxGastoVR.Location = new System.Drawing.Point(51, 259);
             this.checkBoxGastoVR.Name = "checkBoxGastoVR";
             this.checkBoxGastoVR.Size = new System.Drawing.Size(149, 29);
             this.checkBoxGastoVR.TabIndex = 64;
             this.checkBoxGastoVR.Text = "Gastos em VR";
             this.checkBoxGastoVR.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(124)))), ((int)(((byte)(243)))));
-            this.label5.Location = new System.Drawing.Point(46, 83);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(294, 32);
-            this.label5.TabIndex = 72;
-            this.label5.Text = "Exibição de Informações";
             // 
             // label3
             // 
@@ -157,7 +97,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(124)))), ((int)(((byte)(243)))));
-            this.label3.Location = new System.Drawing.Point(46, 230);
+            this.label3.Location = new System.Drawing.Point(46, 132);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(197, 32);
             this.label3.TabIndex = 73;
@@ -175,21 +115,16 @@
             this.btnGerar.TabIndex = 74;
             this.btnGerar.Text = "Gerar Relatório";
             this.btnGerar.UseVisualStyleBackColor = false;
-            this.btnGerar.Click += new System.EventHandler(this.button7_Click);
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
             // 
             // f_Relatorio
             // 
             this.ClientSize = new System.Drawing.Size(682, 575);
             this.Controls.Add(this.btnGerar);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.checkBoxGastoVR);
             this.Controls.Add(this.checkBoxGastoVT);
             this.Controls.Add(this.checkBoxGastoINSS);
-            this.Controls.Add(this.checkBoxGastoLiquido);
-            this.Controls.Add(this.checkBoxGastoBruto);
-            this.Controls.Add(this.checkBoxIDFuncionario);
-            this.Controls.Add(this.checkBoxNomeFuncionario);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label10);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -208,14 +143,9 @@
         #endregion
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.CheckBox checkBoxNomeFuncionario;
-        private System.Windows.Forms.CheckBox checkBoxIDFuncionario;
-        private System.Windows.Forms.CheckBox checkBoxGastoBruto;
-        private System.Windows.Forms.CheckBox checkBoxGastoLiquido;
         private System.Windows.Forms.CheckBox checkBoxGastoINSS;
         private System.Windows.Forms.CheckBox checkBoxGastoVT;
         private System.Windows.Forms.CheckBox checkBoxGastoVR;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnGerar;
     }
--- Forms/f_Relatorio.cs
+++ Forms/f_Relatorio.cs
@@ -1,28 +1,321 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace SisRH_Desktop
 {
     public partial class f_Relatorio : Form
     {
+
+        private SqlConnection con;
+        private SqlCommand cmd;
+
+        private readonly string url = @"Data Source=DESKTOP-DNIRGUH\SQLEXPRESS;Initial Catalog=BD_Funcionario;"
+        + "Integrated Security=true;";
+
         public f_Relatorio()
         {
             InitializeComponent();
+         
         }
 
-        private void button7_Click(object sender, EventArgs e)
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            relatorioOficial();
+
+
+        }
+
+
+
+        private void GerarRelatorio()
+        {
+            string query = @"SELECT 
+                                SUM(TB_funcionario.salario_bruto) AS TotalSalarioBruto, 
+                                SUM(TB_Holerite.SalarioLiquido) AS TotalSalarioLiquido, 
+                                SUM(TB_Holerite.desconto_inss) AS TotalINSS, 
+                                SUM(TB_Holerite.desconto_vt) AS TotalVT                                 
+                            FROM TB_funcionario 
+                            JOIN TB_Holerite ON TB_funcionario.registro = TB_Holerite.id_funcionario;";
+
+            using (SqlConnection connection = new SqlConnection(url))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+
+                try
+                {
+                    connection.Open();
+                    adapter.Fill(dataTable);
+
+                    ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                    using (ExcelPackage package = new ExcelPackage())
+                    {
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Relatório");
+
+                        // Escrevendo os totais na planilha
+                        worksheet.Cells["A1"].Value = "Total Salário Bruto";
+                        worksheet.Cells["B1"].Value = dataTable.Rows[0]["TotalSalarioBruto"];
+
+                        worksheet.Cells["A2"].Value = "Total Salário Líquido";
+                        worksheet.Cells["B2"].Value = dataTable.Rows[0]["TotalSalarioLiquido"];
+
+                        worksheet.Cells["A3"].Value = "Total INSS";
+                        worksheet.Cells["B3"].Value = dataTable.Rows[0]["TotalINSS"];
+
+                        worksheet.Cells["A4"].Value = "Total VT";
+                        worksheet.Cells["B4"].Value = dataTable.Rows[0]["TotalVT"];
+
+                       
+                        using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                        {
+                            saveFileDialog.Filter = "Arquivos Excel (*.xlsx)|*.xlsx";
+                            saveFileDialog.FileName = "RelatorioFuncionarios.xlsx";
+
+                            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                            {
+                                FileInfo excelFile = new FileInfo(saveFileDialog.FileName);
+                                package.SaveAs(excelFile);
+                            }
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+        private void relatorioOficial()
+        {
+            string query = @"SELECT TB_funcionario.registro AS IDFuncionario,
+                            TB_funcionario.nome AS NomeFuncionario,
+                            TB_Holerite.SalarioBruto,
+                            TB_Holerite.SalarioLiquido";
+
+            bool hasINSS = checkBoxGastoINSS.Checked;
+            bool hasVT = checkBoxGastoVT.Checked;
+            bool hasVR = checkBoxGastoVR.Checked;
+
+            if (hasINSS)
+                query += ", TB_Holerite.desconto_inss";
+
+            if (hasVT)
+                query += ", TB_funcionario.valor_vt";
+
+            if (hasVR)
+                query += ", TB_funcionario.valor_vr";
+
+            query += @"
+                FROM TB_funcionario
+                JOIN TB_Holerite ON TB_funcionario.registro = TB_Holerite.id_funcionario";
+
+            // Configuração da conexão com o banco de dados
+            //string connectionString = "SUA_STRING_DE_CONEXAO_AQUI";
+
+            using (SqlConnection connection = new SqlConnection(url))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+
+                try
+                {
+                    connection.Open();
+                    adapter.Fill(dataTable);
+
+                    ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                    using (ExcelPackage package = new ExcelPackage())
+                    {
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Relatório");
+
+                        // Escrever os dados na planilha
+                        int rowCount = 1;
+                        int colCount = 1;
+
+                        // Escrever cabeçalhos das colunas
+                        foreach (DataColumn column in dataTable.Columns)
+                        {
+                            worksheet.Cells[rowCount, colCount].Value = column.ColumnName;
+                            colCount++;
+                        }
+
+                        rowCount++;
+
+                        // Escrever dados dos funcionários
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            colCount = 1;
+                            foreach (DataColumn column in dataTable.Columns)
+                            {
+                                worksheet.Cells[rowCount, colCount].Value = row[column.ColumnName];
+                                colCount++;
+                            }
+                            rowCount++;
+                        }
+
+                        // Calcular e escrever as somas no final das colunas de INSS, VT e VR
+                        int lastRow = rowCount - 1;
+
+                        if (hasINSS)
+                        {
+                            worksheet.Cells[lastRow + 1, colCount - 1].Value = "Soma Total INSS:";
+                            worksheet.Cells[lastRow + 1, colCount].Formula = $"SUM({GetExcelColumnName(colCount)}2:{GetExcelColumnName(colCount)}{lastRow})";
+                        }
+
+                        if (hasVT)
+                        {
+                            colCount++;
+                            worksheet.Cells[lastRow + 1, colCount - 1].Value = "Soma Total VT:";
+                            worksheet.Cells[lastRow + 1, colCount].Formula = $"SUM({GetExcelColumnName(colCount)}2:{GetExcelColumnName(colCount)}{lastRow})";
+                        }
+
+                        if (hasVR)
+                        {
+                            colCount++;
+                            worksheet.Cells[lastRow + 1, colCount - 1].Value = "Soma Total VR:";
+                            worksheet.Cells[lastRow + 1, colCount].Formula = $"SUM({GetExcelColumnName(colCount)}2:{GetExcelColumnName(colCount)}{lastRow})";
+                        }
+
+                        // Salvar a planilha Excel
+                        using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                        {
+                            saveFileDialog.Filter = "Arquivos Excel (*.xlsx)|*.xlsx";
+                            saveFileDialog.FileName = "RelatorioFuncionarios.xlsx";
+
+                            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                            {
+                                FileInfo excelFile = new FileInfo(saveFileDialog.FileName);
+                                package.SaveAs(excelFile);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        // Função auxiliar para obter o nome da coluna do Excel
+        private string GetExcelColumnName(int columnIndex)
+        {
+            int dividend = columnIndex;
+            string columnName = string.Empty;
+
+            while (dividend > 0)
+            {
+                int modulo = (dividend - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
+                dividend = (dividend - modulo) / 26;
+            }
+
+            return columnName;
+        }
+    
+        private void GerarRelatorioChekbox()
+        {
+            string query = @"SELECT TB_funcionario.registro AS IDFuncionario,
+                                    TB_funcionario.nome AS NomeFuncionario,
+                                    TB_Holerite.SalarioBruto,
+                                    TB_Holerite.SalarioLiquido
+                            FROM TB_funcionario
+                            JOIN TB_Holerite ON TB_funcionario.registro = TB_Holerite.id_funcionario";
+
+            // Configuração da conexão com o banco de dados
+            //string connectionString = "SUA_STRING_DE_CONEXAO_AQUI";
+
+            using (SqlConnection connection = new SqlConnection(url))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+
+                try
+                {
+                    connection.Open();
+                    adapter.Fill(dataTable);
+
+                    ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                    using (ExcelPackage package = new ExcelPackage())
+                    {
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Relatório");
+
+                        // Escrever os dados na planilha
+                        int rowCount = 1;
+                        int colCount = 1;
+
+                        // Escrever cabeçalhos das colunas
+                        foreach (DataColumn column in dataTable.Columns)
+                        {
+                            worksheet.Cells[rowCount, colCount].Value = column.ColumnName;
+                            colCount++;
+                        }
+
+                        rowCount++;
+
+                        // Escrever dados dos funcionários
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            colCount = 1;
+                            foreach (DataColumn column in dataTable.Columns)
+                            {
+                                worksheet.Cells[rowCount, colCount].Value = row[column.ColumnName];
+                                colCount++;
+                            }
+                            rowCount++;
+                        }
+
+                        // Calcular e escrever as somas no final das colunas SalarioBruto e SalarioLiquido
+                        worksheet.Cells[rowCount, 1].Value = "Soma Total Salário Bruto:";
+                        worksheet.Cells[rowCount, 3].Formula = $"SUM(C2:C{rowCount - 1})";
+
+                        worksheet.Cells[rowCount, 4].Value = "Soma Total Salário Líquido:";
+                        worksheet.Cells[rowCount, 5].Formula = $"SUM(D2:D{rowCount - 1})";
+
+                        // Salvar a planilha Excel
+                        using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                        {
+                            saveFileDialog.Filter = "Arquivos Excel (*.xlsx)|*.xlsx";
+                            saveFileDialog.FileName = "RelatorioFuncionarios.xlsx";
+
+                            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                            {
+                                FileInfo excelFile = new FileInfo(saveFileDialog.FileName);
+                                package.SaveAs(excelFile);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+        private void f_Relatorio_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void f_Relatorio_Load(object sender, EventArgs e)
+        private void checkBoxGastoINSS_CheckedChanged(object sender, EventArgs e)
         {
 
         }
